e7ed709ce244ad35dd02744b8d42e89ffde16953
mark.yan <m15901368529@gmail.com>
2023-03-31 15:31:00 +0800

    protobuf使用
    使用protoc命令进行转换go文件，将定义的数据结构进行二进制流和json格式转换

--- hello.go
+++ hello.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	trippb "server/proto/gen/go"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	trip := trippb.Trip{
+		Start:       "abc",
+		End:         "edb",
+		DurationSec: 3600,
+		FeeCent:     1000,
+	}
+	fmt.Println(&trip)
+
+	b, err := proto.Marshal(&trip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%X\n", b)
+
+	var trip2 trippb.Trip
+	err = proto.Unmarshal(b, &trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip2)
+
+	b, err = json.Marshal(&trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
 }
