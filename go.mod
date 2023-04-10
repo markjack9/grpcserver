4ed4dd699c116cfd432ca8823fb4d1700e16269a
mark.yan <m15901368529@gmail.com>
2023-04-10 10:29:12 +0800

    数据类型优化
    json格式数据显示字段的驼峰格式化传输

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module server
 go 1.19
 
 require (
-	google.golang.org/grpc v1.53.0
+	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
-	google.golang.org/genproto v0.0.0-20230223222841-637eb2293923 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230403163135-c38d8f061ccd // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
+	"net/http"
 	trippb "server/proto/gen/go"
 	trip "server/tripservice"
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
+	go startGRPCGateway()
 	lis, err := net.Listen("tcp", ":8081")
 	//建立一个tcp监听地址
 	if err != nil {
@@ -16,7 +22,37 @@ func main() {
 
 	}
 	s := grpc.NewServer()
-	trippb.RegisterTripServiceServer(s, &trip.Service{})
+	trippb.RegisterTripServiceServer(
+		s,
+		&trip.Service{},
+	)
 	//使用grpc架构启动服务
 	log.Fatal(s.Serve(lis))
+
+}
+
+func startGRPCGateway() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+
+	mux := runtime.NewServeMux(runtime.WithMarshalerOption(
+		runtime.MIMEWildcard, &runtime.JSONPb{
+
+			MarshalOptions: protojson.MarshalOptions{
+				UseProtoNames: true,
+			}}))
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(
+		c,
+		mux,
+		"localhost:8081",
+		[]grpc.DialOption{grpc.WithInsecure()},
+	)
+	if err != nil {
+		log.Fatalf("cannot start grpc gateway: %v", err)
+	}
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("cannot listen and server: %v", err)
+	}
 }
